33a04fa1c2e09806263a86dba199c723a2153b93
hsgdoo <h-sharks@hanmail.net>
2022-04-17 23:46:38 +0900

    2. 로직 수행 부분   - 루프에서 실행할 로직 함수 추가 (Strike, Ball 판단부)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,31 +17,45 @@ public class Application {
         ArrayList<Integer> res = new ArrayList<>();
         for(int i = 0; i < question.length(); i++) {
             strike += Strike(question.charAt(i), input.charAt(i));
-            ball += Ball(question.charAt(i), input.charAt(i));
+            ball += Ball(question, input.charAt(i), i);
         }
         res.add(strike);
         res.add(ball);
-        return 0; // 출력부 만들기 전 임시로직
+        System.out.println("stri : " + strike + "bal : " + ball);
+        //임시 삭제예정
+        if(strike == 3) {
+            return 0;
+        } else return 1; // 출력부 만들기 전 임시로직
     }
 
     // 스트라이크 개수 판별
     private static int Strike(char que, char in) {
+        if(que == in) {
+            return 1;
+        }
         return 0;
     }
     // 볼 개수 판별
-    private static int Ball(char que, char in) {
+    private static int Ball(String que, char in, int num) {
+        if((que.indexOf(in) >= 0) &&(que.indexOf(in) != num)) {
+            return 1;
+        }
         return 0;
     }
 
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         int keep = 1;
+        //String question = Integer.toString(Randoms.pickNumberInRange(100, 1000));
+        String question = "125";
+        String input = "";
 
         while(keep == 1) {
-            String question = Integer.toString(Randoms.pickNumberInRange(100, 1000));
+
             System.out.print("숫자를 입력해주세요 : ");
-            String input = Console.readLine();
+            input = Console.readLine();
 
             keep = Solution(question, input);
 
